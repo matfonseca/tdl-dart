48bf93a770cdfc85f025df611854399640a6576f
pastine <casimiropastine@gmail.com>
2021-01-13 20:03:34 -0300

    add onLike and onPass for looking for more dogs

--- App/lib/screens/dog_swiper.dart
+++ App/lib/screens/dog_swiper.dart
@@ -1,12 +1,17 @@
 import 'package:App/common/app_bar.dart';
 import 'package:App/models/dog.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class LikeButton extends StatelessWidget {
+  final onPress;
+
+  LikeButton({this.onPress});
+
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      onPressed: () {},
+      onPressed: () {onPress();},
       elevation: 3.0,
       fillColor: Colors.white, 
       child: Icon(
@@ -21,10 +26,14 @@ class LikeButton extends StatelessWidget {
 }
 
 class RejectButton extends StatelessWidget {
+  final onPress;
+
+  RejectButton({this.onPress});
+
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      onPressed: () {},
+      onPressed: () {onPress();},
       elevation: 3.0,
       fillColor: Colors.white,
       child: Icon(
@@ -85,10 +94,33 @@ class DogInfo extends StatelessWidget {
   }
 }
 
+class DogProfilePicture extends StatelessWidget {
+  final pictureUrl;
+
+  DogProfilePicture({this.pictureUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[50],
+      width: 375,
+      height: 390,
+      child: CachedNetworkImage(
+        imageUrl: this.pictureUrl,
+        placeholder: (context, url) => Container(child: CircularProgressIndicator(), width: 50, height: 50),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+      ),
+    );
+  }
+}
+
 class DogProfile extends StatelessWidget {
   final Dog dog;
 
-  DogProfile({this.dog});
+  final onLike;
+  final onPass;
+
+  DogProfile({this.dog, this.onLike, this.onPass});
 
   @override
   Widget build(BuildContext context) {
@@ -96,13 +128,13 @@ class DogProfile extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Image.asset('images/funny_dog.png'),
+          DogProfilePicture(pictureUrl: this.dog.imageUri),
           DogInfo(dog: dog),
           ButtonBar(
             alignment: MainAxisAlignment.spaceAround,
             children: [
-              RejectButton(),
-              LikeButton(),
+              RejectButton(onPress: onPass),
+              LikeButton(onPress: onLike),
             ],
           ),
         ],
@@ -112,9 +144,20 @@ class DogProfile extends StatelessWidget {
 }
 
 class DogSwiper extends StatefulWidget {
-  final List<Dog> dogs = [new Dog(
-    'Pepe', 'Caniche', 3, 'pequeño', 'todas', 'Es un perro cariñoso, sacado de la calle.', 'fake-uri'
-  )];
+  final List<Dog> dogs = [
+    new Dog(
+      'Pepe', 'Caniche', 3, 'pequeño', 'todas', 'Es un perro cariñoso, sacado de la calle.', 'https://www.washingtonpost.com/resizer/uwlkeOwC_3JqSUXeH8ZP81cHx3I=/arc-anglerfish-washpost-prod-washpost/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg'
+    ),
+    new Dog(
+      'Pepa', 'Caniche', 3, 'pequeño', 'todas', 'Es un perro cariñoso, sacado de la calle.', 'https://www.cesarsway.com/wp-content/uploads/2019/10/AdobeStock_190562703.jpeg'
+    ),
+    new Dog(
+      'Popi', 'Caniche', 3, 'pequeño', 'todas', 'Es un perro cariñoso, sacado de la calle.', 'https://www.dogstrust.org.uk/help-advice/_images/164742v800_puppy-1.jpg'
+    ),
+    new Dog(
+      'Marta', 'Caniche', 3, 'pequeño', 'todas', 'Es un perro cariñoso, sacado de la calle.', 'https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg'
+    ),
+  ];
 
   // DogSwiper({this.dogs});
 
@@ -125,13 +168,24 @@ class DogSwiper extends StatefulWidget {
 class _DogSwiper extends State<DogSwiper> {
   final List<Dog> dogs;
 
+  int _dogIndex = 0;
+
+  void _onLike() {
+    setState(() {
+      _dogIndex++;
+      if (_dogIndex == dogs.length) {
+        _dogIndex = 0;
+      }
+    });
+  }
+
   _DogSwiper({this.dogs});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: "Adoptar"),
-      body: DogProfile(dog: dogs[0]),
+      body: DogProfile(dog: dogs[_dogIndex], onLike: _onLike, onPass: _onLike),
     );
   }
 }
