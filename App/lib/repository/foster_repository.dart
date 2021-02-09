25e78182f4c0e18166c37a7096fee8f386e396e2
pastine <casimiropastine@gmail.com>
2021-02-09 14:50:13 -0300

    add: method in foster repo to get all fosters who liked a dog

--- App/lib/repository/foster_repository.dart
+++ App/lib/repository/foster_repository.dart
@@ -1,5 +1,19 @@
+import 'package:App/models/foster.dart';
+import 'package:App/models/like.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firestore.dart';
+import 'like_repository.dart';
 
 class FosterRepository extends FireStore {
   String collectionName = "Fosters";
+
+  Future<List<Foster>> getFostersWhoLikedDog(dogId) async {
+    LikesRepository likes = LikesRepository();
+    List<Like> dogLikes = await likes.getLikesForDog(dogId);
+    Set<String> fosterIds = Set.from(dogLikes.map((like) => like.fosterId).toList());
+    List<QueryDocumentSnapshot> fosterDocs = await this.getAll();
+    return fosterDocs.where((doc) => fosterIds.contains(doc['id'])).map((doc) => Foster(
+      doc['name'], doc['age'], doc['phone'], doc['amountRoommates'], doc['homeType'], doc['hasBackyard'], doc['hasOtherPets']
+    ));
+  }
 }
--- App/lib/repository/like_repository.dart
+++ App/lib/repository/like_repository.dart
@@ -1,4 +1,5 @@
 import 'package:App/models/like.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'firestore.dart';
 
@@ -7,5 +8,14 @@ class LikesRepository extends FireStore {
 
   Future<String> saveLike(dogId, fosterId) async {
     this.save(Like(dogId: dogId, fosterId: fosterId));
+  }
+
+  Future<List<Like>> getLikesForDog(dogId) async {
+    List<QueryDocumentSnapshot> allLikes = await this.getAll();
+    return allLikes.where((
+      likeDocument) => likeDocument['dogId'] == dogId
+    ).map(
+      (data) => Like(dogId: data['dogId'], fosterId: data['fosterId'])
+    ).toList();
   }
 }
